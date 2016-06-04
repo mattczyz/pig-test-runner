d585f7a52d724eb1cb0a083505df7e24f91edb9c
Matt Czyz <matt.czyz@gmail.com>
2016-06-04 09:07:49 +0100

    Initial hive metastore launcher

--- src/test/java/pig/testing/runner/HiveExecutor.java
+++ src/test/java/pig/testing/runner/HiveExecutor.java
@@ -1,7 +1,10 @@
 package pig.testing.runner;
 
 import org.apache.hadoop.hive.cli.CliDriver;
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.metastore.MetaStoreUtils;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.thrift.HadoopThriftAuthBridge;
 
 
 public class HiveExecutor {
@@ -17,6 +20,19 @@ public class HiveExecutor {
             
         } catch (Exception e) {
             throw new HiveException();
+        }
+    }
+    
+    // TODO: startMetastore needs completing
+    public static void startMetastore() {
+        HiveConf configuration = new HiveConf();
+        try {
+            MetaStoreUtils.startMetaStore(MetaStoreUtils.findFreePort(), new HadoopThriftAuthBridge(), 
+                    configuration);
+
+        } catch (Throwable e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
